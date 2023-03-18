ce9168b82b2ca67cda65256019f535ac0d0df11f
DoctorDalek1963 <dyson.dyson@icloud.com>
2023-03-18 15:54:16 +0000

    Revert "Hide panel in middle of screen"
    This reverts commit 38b5c2788b1ef2497dfd597b2fa7db16a5705865.

--- src/ika/colororacle/Screen.java
+++ src/ika/colororacle/Screen.java
@@ -124,11 +124,7 @@ final class Screen {
             createSimulationWindow(colorOracle);
         }
         simulationWindow.setImage(simulationImage);
-
-        // NOTE: This line would display the label panel in the middle of the
-        // screen. I couldn't be bothered to offset it to the bottom corner or
-        // something, so I just commented it out
-        //simulationWindow.setPanel(panel);
+        simulationWindow.setPanel(panel);
 
         // Bring our application to the foreground. This discussion is for
         // frames, not dialogs:
